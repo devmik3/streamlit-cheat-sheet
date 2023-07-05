e92b5c2116e7ddfd4c4aaca47b31876d747063ca
devmik3 <135856594+devmik3@users.noreply.github.com>
2023-07-05 13:55:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ Contributors:
 """
 
 import streamlit as st
-from pathlib import Path
+## from pathlib import Path
 import base64
 
 # Initial page config
@@ -39,10 +39,10 @@ def main():
 
 # Thanks to streamlitopedia for the following code snippet
 
-def img_to_bytes(img_path):
-    img_bytes = Path(img_path).read_bytes()
-    encoded = base64.b64encode(img_bytes).decode()
-    return encoded
+##def img_to_bytes(img_path):
+  ##  img_bytes = Path(img_path).read_bytes()
+    ##encoded = base64.b64encode(img_bytes).decode()
+    ##return encoded
 
 # sidebar
 
